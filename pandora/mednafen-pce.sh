545f6e6d21dbeafa9aa19938305ef3e2e8ce0fe2
Paul Dersey <pdersey@gmail.com>
2011-05-14 15:08:18 -0400

    add pandora directory

---
+++ pandora/mednafen-pce.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+export LD_LIBRARY_PATH=`pwd`/lib
+export SDL_VIDEODRIVER=omapdss
+export SDL_OMAP_LAYER_SIZE=684x464
+export SDL_OMAP_VSYNC=1
+export MEDNAFEN_HOME=`pwd`/.mednafen
+
+if [ ! -d .mednafen ] ; then
+    cp -r mednafen-default .mednafen
+fi
+
+./mednafen -force_module pce_fast "$FILE"
