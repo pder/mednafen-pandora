cc8026a00683160e4e58170d10acb901a35a025e
Paul Dersey <pdersey@gmail.com>
2011-05-15 18:05:00 -0400

    add gba launch script

---
+++ pandora/mednafen-gba.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+export LD_LIBRARY_PATH=`pwd`/lib
+export SDL_VIDEODRIVER=omapdss
+export SDL_OMAP_LAYER_SIZE=720x480
+export SDL_OMAP_VSYNC=1
+export MEDNAFEN_HOME=`pwd`/.mednafen
+
+if [ ! -d .mednafen ] ; then
+    cp -r mednafen-default .mednafen
+fi
+
+./mednafen "$1"
