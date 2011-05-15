33e1fd45ef9d91142436b34e4601be9714d3f140
Paul Dersey <pdersey@gmail.com>
2011-05-15 19:20:27 -0400

    update resolution for gb

--- pandora/mednafen-gb.sh
+++ pandora/mednafen-gb.sh
@@ -2,7 +2,7 @@
 
 export LD_LIBRARY_PATH=`pwd`/lib
 export SDL_VIDEODRIVER=omapdss
-export SDL_OMAP_LAYER_SIZE=684x464
+export SDL_OMAP_LAYER_SIZE=480x432
 export SDL_OMAP_VSYNC=1
 export MEDNAFEN_HOME=`pwd`/.mednafen
 
